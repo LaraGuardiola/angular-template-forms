40dcb3f228ca07a96080f1639cf5f7e4c3ff51f7
Sergio <webdev.slara@gmail.com>
2022-05-19 13:42:55 +0200

    imported FormsModule (template forms) and tested two way binding with ngModel directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailform';
+  email!: string
+
+  constructor(){
+    setInterval(()=> {
+      this.email += 'a'
+    },1000)
+  }
 }
+
+//since email its two way binded in the template, every time email gets updated, it will also update the template as well
